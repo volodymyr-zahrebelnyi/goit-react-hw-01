e97e51e89928fb58032ce0044f4007a119b764ac
Volodymyr Zahrebelnyi <vov41kttt@gmail.com>
2024-08-11 21:29:38 +0300

    hw1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import clsx from "clsx";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import App from "./components/App/App.jsx";
+import "../src/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
